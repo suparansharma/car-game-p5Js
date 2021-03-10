d35e01187a9aaf662a3e4efa897cab98144b691b
rashik69 <47859688+rashik69@users.noreply.github.com>
2021-03-10 19:13:07 +0600

    obstacles & sketch

--- sketch.js
+++ sketch.js
@@ -17,9 +17,19 @@ function draw()
     background(100,200,300);
     for(let i=0;i<obs.length;i++)
     {
+        if(obs[i].collides(car))
+        {
+            console.log('collision');
+            noLoop();
+        }
     
     obs[i].update();
     obs[i].show();
+
+    if(obs[i].isOutOfScreen())
+    {
+        obs.splice(i,1);
+    }
 
     }
     car.show();
